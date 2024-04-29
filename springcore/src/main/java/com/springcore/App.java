20857dff472672d4ac7cf016bb80d97ca848fd3f
suryanshhbtu <suryanshhbtu@gmail.com>
2024-04-29 14:46:44 +0530

    Injecting Collections list, set, map, properties

--- springcore/src/main/java/com/springcore/App.java
+++ springcore/src/main/java/com/springcore/App.java
@@ -1,8 +1,13 @@
 package com.springcore;
 
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.springcore.collections.Employee;
 import com.springcore.entity.Student;
 
 /**
@@ -14,12 +19,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World !" );
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
-        Student std = (Student) applicationContext.getBean("Student_1");
-        System.out.println(std);
-        std = (Student) applicationContext.getBean("Student_2");
-        System.out.println(std);
-        std = (Student) applicationContext.getBean("Student_3");
-        System.out.println(std);
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/springcore/collections/collectionconfig.xml");
+
+        Employee ict = (Employee) applicationContext.getBean("Employee_1");
+        System.out.println(ict.toString()); 
     }
 }
