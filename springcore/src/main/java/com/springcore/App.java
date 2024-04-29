04f5ccca7c8f349e873d4eaed21ab94884acbbd5
suryanshhbtu <suryanshhbtu@gmail.com>
2024-04-29 15:58:54 +0530

    Injecting Reference Type - "p:schema"

--- springcore/src/main/java/com/springcore/App.java
+++ springcore/src/main/java/com/springcore/App.java
@@ -1,14 +1,9 @@
 package com.springcore;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
-
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
-import com.springcore.collections.Employee;
-import com.springcore.entity.Student;
 import com.springcore.ref.A;
 
 /**
@@ -20,9 +15,16 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World !" );
-        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/springcore/ref/refconfig.xml");
-
-        A a = (A) applicationContext.getBean("aref");
-        System.out.println(a); 
+        try(ConfigurableApplicationContext applicationContext = new ClassPathXmlApplicationContext("com/springcore/ref/refconfig.xml")){
+        	A a = (A) applicationContext.getBean("aref");
+        	System.out.println(a); 
+        }catch (Exception e) {
+        	e.printStackTrace();
+			// TODO: handle exception
+		}
+        
+        
+        
+        
     }
 }
