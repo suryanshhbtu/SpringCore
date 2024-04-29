d9710aaeed0140d6e58a3708e61099f6d42b00fb
suryanshhbtu <suryanshhbtu@gmail.com>
2024-04-29 10:58:45 +0530

    using beans -> bean -> property -> values

---
+++ springcore/src/main/java/com/springcore/App.java
@@ -0,0 +1,21 @@
+package com.springcore;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.springcore.entity.Student;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World !" );
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("config.xml");
+        Student std = (Student) applicationContext.getBean("Student_1");
+        System.out.println(std.toString());
+    }
+}
