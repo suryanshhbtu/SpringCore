c5575e14545963dc9bd84fec55d60a7462e8a44b
suryanshhbtu <suryanshhbtu@gmail.com>
2024-05-23 12:57:16 +0530

    Stereotype Annotation @Scope as singleton, prototype

--- springcore/src/main/java/com/springcore/stereotype/Student.java
+++ springcore/src/main/java/com/springcore/stereotype/Student.java
@@ -4,9 +4,11 @@ package com.springcore.stereotype;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component("abc")
+@Scope("prototype")
 public class Student {
 
 	@Value("Suryansh")
--- springcore/src/main/java/com/springcore/stereotype/Test.java
+++ springcore/src/main/java/com/springcore/stereotype/Test.java
@@ -6,8 +6,11 @@ public class Test {
 
 	public static void main(String[] args) {
 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("com/springcore/stereotype/config.xml");
+
+		Student s1 = context.getBean("abc", Student.class);
+		System.out.println(s1.hashCode());
 		
-		Student s = context.getBean("abc", Student.class);
-		System.out.println(s);
+		Student s2 = context.getBean("abc", Student.class);
+		System.out.println(s2.hashCode());
 	}
 }
